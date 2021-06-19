3f185df3b151a2055f3105b926db870c3be999c3
ibrahim saleh <isaleh@tetco.sa>
2021-06-19 22:02:15 +0300

    first commit

---
+++ src/components/Task.vue
@@ -0,0 +1,43 @@
+<template>
+<div @dblclick="$emit('toggle-reminder',task.id)">
+<div :class="[task.reminder?'reminder':'','task']" >
+    <h3>{{task.text}}
+        <!-- <i class="fas fa-times" @click="onDeleteTask(task.id)"></i> -->
+    </h3>
+    <p>{{task.day}}</p>
+</div>
+</div>
+</template>
+
+<script>
+    export default{
+        name:"Task",
+        props:{
+            task:Object
+        },
+        methods:{
+          onDeleteTask(id){
+            this.$emit('delete-task',id)
+          }
+        }
+    }
+</script>
+<style scope>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+<div v-for="task in tasks" :key="task.id">
+    <Task :task="task" @delete-task="deleteTask" @toggle-reminder="$emit('toggle-reminder',task.id)"/>
+</div>
+</div>
+</template>
+
+<script>
+    import Task from "./Task.vue"
+    export default{
+        name:'Tasks',
+        props:{
+            tasks:Array
+        },
+        components:{
+            Task
+        },
+       methods:{
+           deleteTask(id)
+           {
+               this.$emit('delete-task',id)
+           }
+       }, 
+
+    }
+</script>
